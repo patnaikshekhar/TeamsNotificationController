58688e1858e357867accc6ccffff3a38fe1ab1cb
Shekhar <patnaikshekhar@gmail.com>
2020-07-28 07:40:45 +0000

    Testing Closure

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ mod kubernetes;
 
 use crate::kubernetes::KubeClient;
 use anyhow::Result;
+use log::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
     let kube_client = KubeClient::new().await?;
 
-    kube_client.watch_events(|event| {}).await?;
+    kube_client.watch_events(|event| {
+        info!("Main - {:?}", event);
+    }).await?;
 
     Ok(())
 }
